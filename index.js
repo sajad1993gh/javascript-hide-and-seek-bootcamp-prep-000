f2bce6f1076e7fccff83c3ddd64b1925d05e6307
Sajad Gholamzadehrizi <sajad1993gh@gmail.com>
2019-01-18 18:42:54 +0000

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,27 @@ function getFirstSelector(selector){
 function nestedTarget(){
   return document.querySelector('#nested .target' )
 }
-function 
+function increaseRankBy(n){
+  const rank = document.querySelectorAll('.ranked-list')
+     const firstList = rank[0]
+      const secondList = rank[1]
+
+      let children = firstList.children
+      let start = 1
+      for (let i = 0, l = rank.length; i < l; i++) {
+        parseInt(rank[i].innerHTML) = start + i + 3 ;
+      }
+/*
+      children = secondList.children
+      start = 12
+
+      for (let i = 0, l = children.length; i < l; i++) {
+        parseInt(children[i].innerHTML) = start - i + 3
+      }
+      */
+}
+
+function deepestChild(){
+     const child = document.querySelector('#grand-node div div div div')
+    console.log(child.innerHTML)
+}
